04625293d990924916b69387c71ee0b51b9dba99
bobur <uzbek.4103@gmail.com>
2022-07-26 18:25:51 +0500

    Task #4. Primitives v1.0

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        int i = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        System.out.println(i);
     }
 }
